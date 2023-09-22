4faf750f17ddd48f566f2e00a66e5de2f736672c
Shrey <shreybirmiwal@gmail.com>
2023-09-22 18:56:43 -0500

    password protection

--- src/Pages/Inventory.js
+++ src/Pages/Inventory.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Inventory() {
-  return (
-    <div>Inventory</div>
-  )
+
+  const [admin, setAdmin] = useState(false)
+
+  if(admin == false){
+    return(
+      <div>
+        Please verify that you are a Locker admin
+      </div>
+    );
+  } else {
+    return(
+      <div>
+        YIPEE
+      </div>
+    );
+  }
+
+ 
+
 }
 
 export default Inventory
