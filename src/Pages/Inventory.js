4d8fa7ad5b50d2a0e47bbab73d9ed23969c18471
Shrey <shreybirmiwal@gmail.com>
2023-09-22 19:16:18 -0500

    Admin addign items

--- src/Pages/Inventory.js
+++ src/Pages/Inventory.js
@@ -1,25 +1,142 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { collection, addDoc } from "firebase/firestore"; // Import necessary Firestore functions
+import { db } from '../firebase';
 
 function Inventory() {
+  const correctPassword = 'PASS'; // REPLACE with the correct password
+  const [admin, setAdmin] = useState(false);
+  const [password, setPassword] = useState('');
+  const [newItemName, setNewItemName] = useState('');
+  const [newStock, setNewStock] = useState('');
 
-  const [admin, setAdmin] = useState(false)
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-  if(admin == false){
-    return(
+  const handleVerifyClick = () => {
+    if (password === correctPassword) {
+      setAdmin(true);
+      toast.success('Log in success', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    } else {
+      toast.error('Incorrect Password', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+  };
+
+  const handleAddNewItem = async () => {
+    if (!admin) {
+      // Check if the user is an admin
+      toast.error('Only admins can add items.', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+      return;
+    }
+
+    if (newItemName && newStock) {
+      try {
+        // Add a new document to the Firestore collection
+        const docRef = await addDoc(collection(db, "inventory"), {
+          [newItemName]: newStock,
+        });
+
+        toast.success('Item added successfully!', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      } catch (error) {
+        console.error('Error adding item to the database: ', error);
+        toast.error('Error adding item to the database.', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      }
+    } else {
+      toast.error('Please enter both item name and stock.', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+  };
+
+  if (admin) {
+    return (
       <div>
-        Please verify that you are a Locker admin
+        <h1>YIPEE</h1>
+        <input
+          type="text"
+          placeholder="Item Name"
+          value={newItemName}
+          onChange={(e) => setNewItemName(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Current Stock"
+          value={newStock}
+          onChange={(e) => setNewStock(e.target.value)}
+        />
+        <button onClick={handleAddNewItem}>Add Item</button>
+        <ToastContainer />
       </div>
     );
   } else {
-    return(
+    return (
       <div>
-        YIPEE
+        <h2>Please verify that you are an official Locker admin</h2>
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button onClick={handleVerifyClick}>Verify</button>
+        <ToastContainer />
       </div>
     );
   }
-
- 
-
 }
 
-export default Inventory
+export default Inventory;
--- src/Pages/Locker.js
+++ src/Pages/Locker.js
@@ -5,132 +5,128 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function Locker() {
-    const [inventoryData, setInventoryData] = useState([]);
-    const [checkoutQuantities, setCheckoutQuantities] = useState({});
-    const [studentGrade, setStudentGrade] = useState(''); 
+  const [inventoryData, setInventoryData] = useState([]);
+  const [checkoutQuantities, setCheckoutQuantities] = useState({});
+  const [studentGrade, setStudentGrade] = useState(''); 
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const querySnapshot = await getDocs(collection(db, "inventory"));
-                var inventoryArray = [];
-                querySnapshot.forEach((doc) => {
-                    const data = doc.data();
-                    const itemName = doc.id;
-                    const stockAmount = data.Amount;
-                    inventoryArray.push({ itemName, stockAmount });
-                });
-                setInventoryData(inventoryArray);
-            } catch (error) {
-                console.error('Error fetching inventory data: ', error);
-            }
-        };
-        fetchData();
-    }, []);
-
-    const handleQuantityChange = (itemName, quantity) => {
-        setCheckoutQuantities({ ...checkoutQuantities, [itemName]: quantity });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "inventory"));
+        var inventoryArray = [];
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          const itemName = doc.id;
+          const stockAmount = data.Amount;
+          inventoryArray.push({ itemName, stockAmount });
+        });
+        setInventoryData(inventoryArray);
+      } catch (error) {
+        console.error('Error fetching inventory data: ', error);
+      }
     };
+    fetchData();
+  }, []);
 
-    const handleStudentGradeChange = (e) => {
-        const grade = e.target.value;
-        setStudentGrade(grade);
-    };
+  const handleQuantityChange = (itemName, quantity) => {
+    setCheckoutQuantities({ ...checkoutQuantities, [itemName]: quantity });
+  };
 
-    const handleSubmit = async () => {
-        // Check if grade entered
-        if (studentGrade !== '') {
-            console.log("Checkout quantities:", checkoutQuantities);
-            console.log("Student's Grade:", studentGrade);
-    
-            // Loop through checkout quantities and update the database
-            for (const itemName in checkoutQuantities) {
-                if (checkoutQuantities.hasOwnProperty(itemName)) {
-                    const quantityToCheckout = checkoutQuantities[itemName];
-                    console.log(itemName + " " + quantityToCheckout)
+  const handleStudentGradeChange = (e) => {
+    const grade = e.target.value;
+    setStudentGrade(grade);
+  };
 
-                    const docRef = doc(db, "inventory", itemName);
-                    await updateDoc(docRef, {
-                        Amount: increment(-1*quantityToCheckout)
-                    });
+  const handleSubmit = async () => {
+    // Check if grade entered
+    if (studentGrade !== '') {
+      console.log("Checkout quantities:", checkoutQuantities);
+      console.log("Student's Grade:", studentGrade);
 
-                }
-            }
+      // Loop through checkout quantities and update the database
+      for (const itemName in checkoutQuantities) {
+        if (checkoutQuantities.hasOwnProperty(itemName)) {
+          const quantityToCheckout = checkoutQuantities[itemName];
+          console.log(itemName + " " + quantityToCheckout)
 
-            toast.success('Success!', {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "dark",
-            });
+          const docRef = doc(db, "inventory", itemName);
+          await updateDoc(docRef, {
+            Amount: increment(-1*quantityToCheckout)
+          });
 
-
-            setTimeout(() => {
-                window.location.reload();
-            }, 1000);
-
-        } else {
-            
-            toast.warn('Enter a valid grade level', {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "dark",
-            });
         }
-    };
+      }
 
-    return (
-        <div className='flex flex-col items-center'>
-            <ul className='overflow-y-scroll h-96 w-full'>
-                {inventoryData.map((item, index) => (
-                    <li key={index}>
-                        <div className='flex flex-row justify-between bg-gray-200 p-2'>
-                            <h1>{item.itemName}</h1>
-                            <select
-                                onChange={(e) => handleQuantityChange(item.itemName, parseInt(e.target.value))}
-                                value={checkoutQuantities[item.itemName] || 0}
-                            >
-                                {[...Array(item.stockAmount + 1).keys()].map((quantity) => (
-                                    <option key={quantity} value={quantity}>
-                                        {quantity}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>
-                    </li>
+      toast.success('Success!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+
+    } else {
+      toast.warn('Enter a valid grade level', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 w-full">
+      <div className="w-full max-w-xl p-4">
+        <h1 className="text-2xl font-bold mb-4">Locker</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {inventoryData.map((item, index) => (
+            <div key={index} className="border border-gray-300 rounded p-2">
+              <h2 className="text-lg font-semibold">{item.itemName}</h2>
+              <select
+                onChange={(e) => handleQuantityChange(item.itemName, parseInt(e.target.value))}
+                value={checkoutQuantities[item.itemName] || 0}
+              >
+                {[...Array(item.stockAmount + 1).keys()].map((quantity) => (
+                  <option key={quantity} value={quantity}>
+                    {quantity}
+                  </option>
                 ))}
-            </ul>
-
-            <div className="flex flex-row w-full sm:w-56 items-center justify-center bg-gray-200 hover:bg-gray-400 rounded-md shadow-md cursor-pointer">
-                <input
-                    type="text"
-                    placeholder="Student's Grade"
-                    value={studentGrade}
-                    onChange={handleStudentGradeChange}
-                />
+              </select>
             </div>
-
-            <div
-                className={`flex flex-row w-full sm:w-56 items-center justify-center bg-gray-200 hover:bg-gray-400 rounded-md shadow-md cursor-pointer`}
-                onClick={handleSubmit}
-            >
-                <h1 className='p-5 text-2xl font-bold text-center'> Submit </h1>
-            </div>
-
-
-            <ToastContainer/>
-
+          ))}
         </div>
-    )
+        <div className="mt-4">
+          <input
+            type="text"
+            placeholder="Student's Grade"
+            value={studentGrade}
+            onChange={handleStudentGradeChange}
+            className="border border-gray-300 rounded p-2 w-1/3 mr-5"
+          />
+                  <button
+          className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded cursor-pointer"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+        </div>
+
+      </div>
+      <ToastContainer/>
+    </div>
+  );
 }
 
 export default Locker;
