ecb4960bd6e2569dcbdfb767e8dacc59ca7a657b
Shrey <shreybirmiwal@gmail.com>
2023-10-16 17:23:07 -0500

    Scrollable element and added other items to locker

--- src/Pages/Locker.js
+++ src/Pages/Locker.js
@@ -96,40 +96,42 @@ function Locker() {
         <h1 className="text-2xl font-bold mb-4">Locker</h1>
         <p className="text-gray-600 mb-4">Select and update the quantities you have checked out from the Locker so we can keep track of our stockpile.</p>
         <div className="overflow-x-auto">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead>
-              <tr>
-                <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Item Name
-                </th>
-                <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Amount Checked Out
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {inventoryData.map((item, index) => (
-                <tr key={index}>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    {item.itemName}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <select
-                      onChange={(e) => handleQuantityChange(item.itemName, parseInt(e.target.value))}
-                      value={checkoutQuantities[item.itemName] || 0}
-                      className="border border-gray-300 rounded p-2"
-                    >
-                      {[...Array(item.stockAmount + 1).keys()].map((quantity) => (
-                        <option key={quantity} value={quantity}>
-                          {quantity}
-                        </option>
-                      ))}
-                    </select>
-                  </td>
+          <div style={{ maxHeight: '400px', overflowY: 'auto' }}>
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead>
+                <tr>
+                  <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Item Name
+                  </th>
+                  <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Amount Checked Out
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {inventoryData.map((item, index) => (
+                  <tr key={index}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {item.itemName}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <select
+                        onChange={(e) => handleQuantityChange(item.itemName, parseInt(e.target.value))}
+                        value={checkoutQuantities[item.itemName] || 0}
+                        className="border border-gray-300 rounded p-2"
+                      >
+                        {[...Array(item.stockAmount + 1).keys()].map((quantity) => (
+                          <option key={quantity} value={quantity}>
+                            {quantity}
+                          </option>
+                        ))}
+                      </select>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
         <div className="mt-4">
           <input
@@ -150,6 +152,7 @@ function Locker() {
       <ToastContainer />
     </div>
   );
+  
 }
 
 export default Locker;
