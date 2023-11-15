509c80590aabebf829de92f2fe76df3cf8efc5f2
Shrey <shreybirmiwal@gmail.com>
2023-11-14 19:02:36 -0600

    test w/o env|

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import {getAuth} from "firebase/auth"
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIWekN2FbpnGigKU9JVekPlrPfO-a6OVE",
-  authDomain: "locker-stock-management.firebaseapp.com",
-  projectId: "locker-stock-management",
-  storageBucket: "locker-stock-management.appspot.com",
-  messagingSenderId: "391715081944",
-  appId: "1:391715081944:web:9e85bd15e4f4a6d73b11ad"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
