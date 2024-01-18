2f78fcf61ab83b71b1398f7a016d1fe8770b4824
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-01-17 19:30:23 -0600

    Inventory table

--- src/Pages/Inventory.js
+++ src/Pages/Inventory.js
@@ -5,6 +5,30 @@ import { collection, addDoc, setDoc, updateDoc, increment, doc, deleteDoc, getDo
 import { db } from '../firebase';
 import { Link } from 'react-router-dom';
 import { AdminNav } from '../Components/AdminNav';
+import { InventoryTable } from '../Components/InventoryTable';
+import {
+  MagnifyingGlassIcon,
+  ChevronUpDownIcon,
+} from "@heroicons/react/24/outline";
+import { PencilIcon, UserPlusIcon } from "@heroicons/react/24/solid";
+import {
+  Card,
+  CardHeader,
+  Input,
+  Typography,
+  Button,
+  CardBody,
+  Chip,
+  CardFooter,
+  Tabs,
+  TabsHeader,
+  Tab,
+  Avatar,
+  IconButton,
+  Tooltip,
+} from "@material-tailwind/react";
+ 
+const TABLE_HEAD = ["Item Name", "Current Stock", "Update", "Delete"];
 
 function Inventory({school, short_school}) {
   
@@ -288,17 +312,17 @@ function Inventory({school, short_school}) {
   };
 
   return (
-      <div>
+      <div className='min-h-screen'>
         <AdminNav school={school} short_school={short_school}/>
 
-        <div>
+        <div class="container mx-auto p-7">
 
-          <h1 className="text-3xl font-semibold mb-2">Inventory Management</h1>
+          <h1 className="text-3xl font-semibold mt-3">Inventory Management</h1>
           {admin ? (
             <div className="mb-4">
-              <h2 className="text-xl font-semibold mb-2 mt-10">Add item to database</h2>
+              <h2 className="text-xl font-semibold mb-2 mt-6">Add item to database</h2>
 
-              <div className="flex items-center space-x-2">
+              <div className="flex items-center space-x-2 mb-10">
                 <input
                   className="p-2 border border-gray-300 rounded"
                   type="text"
@@ -321,71 +345,129 @@ function Inventory({school, short_school}) {
                 </button>
               </div>
 
-              <div className="mb-4 mt-10">
-                <h2 className="text-xl font-semibold mb-2">All Items</h2>
-                <div className="overflow-y-auto">
-                  <table className="min-w-full divide-y divide-gray-200">
+
+
+
+
+
+
+
+              
+
+              <div>
+                <Card className="h-full w-full">
+
+                <CardHeader floated={false} shadow={false} className="rounded-none">
+                    <div className="w-full md:w-72">
+                      <Input
+                        label="Search"
+                        icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+                      />
+                    </div>
+                </CardHeader>
+
+                <CardBody className="overflow-scroll px-0">
+                  <table className="mt-4 w-full min-w-max table-auto text-left">
                     <thead>
                       <tr>
-                        <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                          Item Name
-                        </th>
-                        <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                          Stock
-                        </th>
-                        <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                          Update Stock
-                        </th>
-                        <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                          Delete Item
-                        </th>
+                        {TABLE_HEAD.map((head, index) => (
+                          <th
+                            key={head}
+                            className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50"
+                          >
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                            >
+                              {head}{" "}
+                              {index !== TABLE_HEAD.length - 1 && (
+                                <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
+                              )}
+                            </Typography>
+                          </th>
+                        ))}
                       </tr>
                     </thead>
-                    <tbody className='overflow-y-scroll h-12'>
-                      {inventoryData.map((item) => (
-                        <tr key={item.itemName}>
-                          <td className="px-6 py-4 whitespace-nowrap">
-                            {item.itemName}
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap">
-                            {item.stockAmount}
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap">
-                            <div className="flex items-center space-x-2">
-                              <input
-                                className="p-2 border border-gray-300 rounded"
-                                type="number"
-                                placeholder="New Stock"
-                                value={itemStocks[item.itemName] || ''}
-                                onChange={(e) =>
-                                  setItemStocks({
-                                    ...itemStocks,
-                                    [item.itemName]: e.target.value,
-                                  })
-                                }
-                              />
-                              <button
-                                className="bg-blue-500 text-white py-1 px-2 rounded hover:bg-blue-600"
-                                onClick={() => handleUpdateStock(item.itemName)}
-                              >
-                                Update
-                              </button>
-                            </div>
-                          </td>
-                          <td className="px-6 py-4 whitespace-nowrap">
-                            <button
-                              className="bg-red-500 text-white py-1 px-2 rounded hover:bg-red-600"
-                              onClick={() => handleDeleteItem(item.itemName)}
-                            >
-                              Delete
-                            </button>
-                          </td>
-                        </tr>
-                      ))}
+                    <tbody>
+                      {inventoryData.map(
+                        ({ itemName, stockAmount }, index) => {
+                          const isLast = index === inventoryData.length - 1;
+                          const classes = isLast
+                            ? "p-4"
+                            : "p-4 border-b border-blue-gray-50";
+
+                          return (
+                            <tr key={itemName}>
+
+                              <td className={classes}>
+                                <div className="flex items-center gap-3">
+                                  <div className="flex flex-col">
+                                    <Typography
+                                      variant="small"
+                                      color="blue-gray"
+                                      className="font-normal"
+                                    >
+                                      {itemName}
+                                    </Typography>
+
+                                  </div>
+                                </div>
+                              </td>
+
+
+                              <td className={classes}>
+                                <div className="flex flex-col">
+                                  <Typography
+                                    variant="small"
+                                    color="blue-gray"
+                                    className="font-normal"
+                                  >
+                                    {stockAmount}
+                                  </Typography>
+
+                                </div>
+                              </td>
+
+
+                              <td className={classes}>
+                                <div className="w-max">
+                                test
+                                </div>
+                              </td>
+
+
+                              <td className={classes}>
+                                <Typography
+                                  variant="small"
+                                  color="blue-gray"
+                                  className="font-normal"
+                                >
+                                    <button
+                                      className="bg-red-500 text-white py-1 px-2 rounded hover:bg-red-600"
+                                      onClick={() => handleDeleteItem(itemName)}
+                                    >
+                                      Delete
+                                    </button>
+                                </Typography>
+                              </td>
+
+
+                            </tr>
+                          );
+                        },
+                      )}
                     </tbody>
                   </table>
-                </div>
+                </CardBody>
+
+                </Card>
               </div>
+
+
+
+
+
             </div>
           ) : (
             <div>
