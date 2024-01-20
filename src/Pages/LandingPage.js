0177d8f49baa51fd579a30555ea28ca59e702c1d
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-01-20 17:17:47 -0600

    Update landing page

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -5,7 +5,7 @@ import { AdminNav } from '../Components/AdminNav';
 const LandingPage = () => (
   <div>
     <div className="text-center mt-16">
-      <h1 className="text-4xl font-bold mb-4">Welcome to trackitnow.xyz by The Locker</h1>
+      <h1 className="text-4xl font-bold mb-4">Welcome to trackitnow.xyz</h1>
       <div>
         <Link to="/whs" className="p-5">Westwood Locker</Link>
         <Link to="/ghs" className="p-5">Georgetown Locker</Link>
